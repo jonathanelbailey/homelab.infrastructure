351cbca6977d638be7a5aeda3184b76399a9e0fd
Jonathan Bailey <jonathan@jelbailey.com>
2023-07-12 16:00:33 -0500

    testing clone only

--- terraform/modules/proxmox/vm/variables.tf
+++ terraform/modules/proxmox/vm/variables.tf
@@ -3,8 +3,13 @@ variable "virtual_machines" {
   default     = {}
   description = "Identifies the object of virtual machines."
 }
-variable "cloud_init_pass" {
-  type        = string
-  default     = "password"
-  description = "Identifies the object of virtual machines."
-}
+# variable "cloud_init_pass" {
+#   type        = string
+#   default     = "password"
+#   description = "Identifies the object of virtual machines."
+# }
+# variable "sshkeys" {
+#   type        = list(string)
+#   default     = []
+#   description = "Identifies the object of virtual machines."
+# }
--- terraform/modules/proxmox/vm/virtual_machine.tf
+++ terraform/modules/proxmox/vm/virtual_machine.tf
@@ -9,36 +9,37 @@ resource "proxmox_vm_qemu" "virtual_machines" {
   ]
   for_each         = var.virtual_machines
   name             = each.value.name
-  qemu_os          = each.value.qemu_os
+  # agent            = each.value.agent
+  # qemu_os          = each.value.qemu_os
   desc             = each.value.description
   target_node      = each.value.target_node
-  os_type          = each.value.os_type
+  # os_type          = each.value.os_type
   full_clone       = each.value.full_clone
   clone            = each.value.template
-  memory           = each.value.memory
-  sockets          = each.value.socket
-  cores            = each.value.cores
-  ssh_user         = each.value.ssh_user
-  sshkeys          = tls_private_key.virtual_machine_keys.public_key_openssh
-  ciuser           = each.value.ssh_user
-  ipconfig0        = "ip=${each.value.ip_address}/32,gw=${each.value.gateway}"
-  cipassword       = var.cloud_init_pass
-  automatic_reboot = each.value.automatic_reboot
-  nameserver       = each.value.dns_servers
+  # memory           = each.value.memory
+  # sockets          = each.value.socket
+  # cores            = each.value.cores
+  # ssh_user         = each.value.ssh_user
+  # sshkeys          = var.sshkeys
+  # ciuser           = each.value.ssh_user
+  # ipconfig0        = "ip=${each.value.ip_address}/32,gw=${each.value.gateway}"
+  # cipassword       = var.cloud_init_pass
+  # automatic_reboot = each.value.automatic_reboot
+  # nameserver       = each.value.dns_servers
 
-  disk {
-    storage = each.value.storage_dev
-    type    = each.value.disk_type
-    size    = each.value.storage
-  }
+  # disk {
+  #   storage = each.value.storage_dev
+  #   type    = each.value.disk_type
+  #   size    = each.value.storage
+  # }
 
-  network {
-    bridge   = each.value.network_bridge_type
-    model    = each.value.network_model
-    mtu      = 0
-    macaddr  = each.value.mac_address
-    queues   = 0
-    rate     = 0
-    firewall = each.value.network_firewall
-  }
+  # network {
+  #   bridge   = each.value.network_bridge_type
+  #   model    = each.value.network_model
+  #   mtu      = 0
+  #   macaddr  = each.value.mac_address
+  #   queues   = 0
+  #   rate     = 0
+  #   firewall = each.value.network_firewall
+  # }
 }
