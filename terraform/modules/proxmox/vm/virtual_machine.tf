22df3e36eddc62e4dfa326f4c6b3cb8fc2b35bc7
Jonathan Bailey <jonathan@jelbailey.com>
2023-07-13 09:23:26 -0500

    test new disk options

--- terraform/modules/proxmox/vm/virtual_machine.tf
+++ terraform/modules/proxmox/vm/virtual_machine.tf
@@ -18,20 +18,33 @@ resource "proxmox_vm_qemu" "deploy_vm" {
   ipconfig0        = var.ipconfig0
   cipassword       = var.cipassword
   oncreate         = var.oncreate
-}
+  bootdisk         = "scsi0"
 
-data "external" "resize_vm_boot_disk" {
-  program = [
-    "bash", "-c", "${path.module}/files/resize_vm_boot_disk.sh", 
-    "scsi0", "${var.boot_disk_size}", "${var.vm_id.id}"]
-}
-
-data "http" "start_vm" {
-  url         = "${var.pm_api_url}/nodes/${var.vm_id.id}/status/start"
-  method      = "POST"
-
-  request_headers = {
-    Authorization       = "PVEAPIToken=${var.pm_api_token_secret}"
-    Content-Type        = "application/json"
+  disk {
+    slot = 0
+    size = "50G"
+    type = "scsi"
+    storage = "local" # Name of storage local to the host you are spinning the VM up on
+    # Enables SSD emulation
+    ssd = 1
+    # Enables thin-provisioning
+    discard = "on"
+    #iothread = 1
   }
 }
+
+# data "external" "resize_vm_boot_disk" {
+#   program = [
+#     "bash", "-c", "${path.module}/files/resize_vm_boot_disk.sh", 
+#     "scsi0", "${var.boot_disk_size}", "${var.vm_id.id}"]
+# }
+
+# data "http" "start_vm" {
+#   url         = "${var.pm_api_url}/nodes/${var.vm_id.id}/status/start"
+#   method      = "POST"
+
+#   request_headers = {
+#     Authorization       = "PVEAPIToken=${var.pm_api_token_secret}"
+#     Content-Type        = "application/json"
+#   }
+# }
