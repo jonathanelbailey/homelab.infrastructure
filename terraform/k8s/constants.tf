d1e4f535e9e39cd88f99ce7572ab578406e1b9b6
Jonathan Bailey <jonathan@jelbailey.com>
2023-07-12 19:02:07 -0500

    change template

--- terraform/k8s/constants.tf
+++ terraform/k8s/constants.tf
@@ -8,7 +8,7 @@ locals {
         os_type             = "cloud-init" # Set to cloud-init to utilize templates
         agent               = 1           # Set to 1 to enable the QEMU Guest Agent. Note, you must run the qemu-guest-agent daemon in the guest for this to have any effect.
         full_clone          = false        # Set to true to create a full clone, or false to create a linked clone. See the docs about cloning for more info. Only applies when clone is set.
-        template            = "k8s" # Name of Template Used to Clone
+        template            = "VM 9000" # Name of Template Used to Clone
         cores               = 16
         socket              = 1
         memory              = 32768
