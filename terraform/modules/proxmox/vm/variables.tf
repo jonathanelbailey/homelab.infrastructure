f244c883667e6f622c848172999145d04eddbf4f
Jonathan Bailey <jonathan@jelbailey.com>
2023-07-12 20:54:09 -0500

    fix variables

--- terraform/modules/proxmox/vm/variables.tf
+++ terraform/modules/proxmox/vm/variables.tf
@@ -1,14 +1,111 @@
-variable "virtual_machines" {
-  type        = map
-  default     = {}
-  description = "Identifies the object of virtual machines."
+variable "name" {
+  type        = string
+  default     = "ubuntu"
+  description = "VM Name"
 }
-variable "cloud_init_user" {
+
+variable "desc" {
+  type        = string
+  default     = ""
+  description = "Short description of VM"
+}
+
+variable "target_node" {
+  type        = string
+  default     = "pve"
+  description = "Node that the VM will be created on"
+}
+
+variable "qemu_os" {
+  type        = string
+  default     = "other"
+  description = "OS of the VM"
+}
+
+variable "os_type" {
+  type        = string
+  default     = "cloud-init"
+  description = "Type of OS"
+}
+
+variable "clone" {
+  type        = string
+  default     = "VM 9000"
+  description = "Name of VM Template to clone VM from"
+}
+
+variable "full_clone" {
+  type        = bool
+  default     = false
+  description = "Full clone or thin VM"
+}
+
+variable "oncreate" {
+  type        = bool
+  default     = false
+  description = "Start VM after creation"
+}
+
+variable "sockets" {
+  type        = number
+  default     = 1
+  description = "Number of CPU sockets"
+}
+
+variable "cores" {
+  type        = number
+  default     = 4
+  description = "Number of cores"
+}
+
+variable "memory" {
+  type        = number
+  default     = 8192
+  description = "Memory in MB"
+}
+
+variable "scsihw" {
+  type        = string
+  default     = "virtio-scsi-pci"
+  description = "SCSI device for boot disk"
+}
+
+variable "disk_size" {
+  type        = string
+  default     = "40G"
+  description = "Size of boot disk"
+}
+
+variable "nic" {
+  type        = string
+  default     = "virtio"
+  description = "Model of Network Interface"
+}
+
+variable "bridge" {
+  type        = string
+  default     = "vmbr0"
+  description = "Bridge Interface for VM"
+}
+
+variable "ipconfig0" {
+  type        = string
+  default     = "ip=dhcp"
+  description = "IP configuration for net adapter #0 on VM"
+}
+
+variable "agent" {
+  type        = number
+  default     = 1
+  description = "QEMU guest agent installed on VM"
+}
+
+variable "ciuser" {
   type        = string
   default     = ""
   description = "Changes the default cloud-init user."
 }
-variable "cloud_init_pass" {
+variable "cipassword" {
   type        = string
   default     = ""
   description = "Changes the default cloud-init user password."
